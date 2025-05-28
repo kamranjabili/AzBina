9380261eee407f7c748d9f1c3e051048a492c2e2
kamranjabili <kamrancebiyev90@gmail.com>
2025-05-28 14:12:57 +0400

    added context

--- src/Presentation/AzBina.WebApi/Program.cs
+++ src/Presentation/AzBina.WebApi/Program.cs
@@ -1,8 +1,15 @@
+using AzBina.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AzBinaDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
